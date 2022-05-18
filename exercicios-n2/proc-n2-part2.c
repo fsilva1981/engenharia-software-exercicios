32aca09d12e8d5d6b9c8058a0ffe928b292eb06c
Francisco Silva <francisco.fsilva@outlook.com>
2022-05-18 00:03:01 -0300

    Implementacao Calculadora 2D
    Concluído parte do exercício porém ficou faltando implementar a calculadora 3D no código.

--- exercicios-n2/proc-n2-part2.c
+++ exercicios-n2/proc-n2-part2.c
@@ -1,33 +1,58 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <locale.h>
+
+// Professor não conseguir concluir o código... vou postar o que conseguir até agora e irei continuar na conclusão.....
 
 // prototipo
 
 int menu_principal();
+int menu_2D();
+int menu_3D();
+
+float quadrilatero(float lado_A, float lado_B);
 
 int main()
-{
 
-    int flag = 0, opcao;
+{
+    setlocale(LC_ALL, "Portuguese");
+
+    int flag = 0, opcao, opcao_2D, opcao_3D;
+
+    float lado_A, lado_B, area;
 
     while (flag == 0)
     {
         opcao = menu_principal();
-        if (opcao == 0)
+        switch (opcao)
 
         {
+        case 0:
 
-            printf("Calculadora 2D\n");
-        }
-        else if (opcao == 1)
+            opcao_2D = menu_2D();
 
-        {
+            if (opcao_2D == 1)
+            {
+                printf("Digite o primeiro lado: ");
+                scanf("\n%f", &lado_A);
+                printf("Digite o segundo lado:");
+                scanf("\n%f", &lado_B);
+
+                area = quadrilatero(lado_A, lado_B);
+                printf("A área é: %.2f\n", area);
+            }
+            else if (opcao_2D == 0)
+            {
+                continue;
+            }
+            break;
+
+        case 1:
             printf("Calculadora 3D\n");
-        }
-        else
+            break;
 
-        {
-            printf("Sistema Encerrado\n");
+        default:
+            printf("Sistema encerrado!\n");
             flag = 1;
         }
     }
@@ -41,11 +66,49 @@ int menu_principal()
 
     int opcao;
 
-    
     printf("Digite 0 para figuras 2D e 1 para figuras 3D\n");
     printf("0 - figuras 2D\n");
     printf("1 - figuras 3D\n");
     printf("2 - Sair do Sistema\n");
+    scanf(" \n%d", &opcao);
+
+    return opcao;
+}
+
+int menu_2D()
+{
+
+    int opcao;
+
+    printf("Calculadora de área de figuras 2D\n");
+    printf("1 - Quadrado/Retângulo\n");
+    printf("2 - Circunferência\n");
+    printf("3 - Triângulo\n");
+    printf("4 - Losango");
+    printf("5 - Paralelogramo");
+    printf("0 - Sair do Sistema\n");
+    scanf(" \n%d", &opcao);
+
+    return opcao;
+}
+
+float quadrilatero(float lado_A, float lado_B)
+{
+    return lado_A * lado_B;
+}
+
+int menu_3D()
+{
+
+    int opcao;
+
+    printf("Calculadora de área de figuras 3D\n");
+    printf("1 - Esfera\n");
+    printf("2 - Cilindro\n");
+    printf("3 - Pirâmide\n");
+    printf("4 - Cone\n");
+    printf("5 - Cubo/Paralepípedo\n");
+    printf("0 - Sair do Sistema\n");
     scanf(" \n%d", &opcao);
 
     return opcao;
